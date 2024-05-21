54f9fa7d38a1f08722cc9bd5033909f3e30b05c8
Andrea Gajelan <103405269+AndreaGajelan@users.noreply.github.com>
2024-05-21 16:18:53 +0800

    Update connection.php
    start date to end date and start time to end time calculations, excluded sundays

--- connection.php
+++ connection.php
@@ -17,15 +17,11 @@ if (isset($_POST['submit'])) {
     $dept = $_POST['dept'];
     $position = $_POST['position'];
     $absenceType = $_POST['absenceType'];
-    $startTime = $_POST['startTime'];
-    $endTime = $_POST['endTime'];
     $pay = $_POST['pay'];
     $leaveType = $_POST['leaveType'];
     $otherLeaveType = $_POST['otherLeaveType'];
-    $startDate = new DateTime($_POST['startDate']);
-    $endDate = new DateTime($_POST['endDate']);
     $reason = $_POST['reason'];
-    $daysToHrs = $_POST['dayToHrs'];
+    $remarks = $_POST['remarks']; // Assuming this is also a field in your form
 
     $host = 'localhost';
     $user = 'root';
@@ -36,24 +32,58 @@ if (isset($_POST['submit'])) {
 
     // LEAVE TYPE N/A
     $naLeaveTypes = ['Vacation Leave', 'Paternity Leave', 'Bereavement Leave', 'Sick Leave', 'Parental Leave'];
-    
     if (in_array($leaveType, $naLeaveTypes)) {
         $otherLeaveType = 'NA'; 
     }
 
-    // TOTAL HOURS BETWEEN START AND END DATE
-    $interval = $startDate->diff($endDate);
-    $dayToHrs = $interval->days * 8 + $interval->h;
+    // Format start and end date with time
+    $startDateTime = new DateTime($_POST['startDate'] . ' ' . $_POST['startTime']);
+    $endDateTime = new DateTime($_POST['endDate'] . ' ' . $_POST['endTime']);
 
-    $startDateFormatted = $startDate->format('Y-m-d H:i:s');
-    $endDateFormatted = $endDate->format('Y-m-d H:i:s');
+    // Calculate total hours
+    
+    // Calculate total hours
+$totalHours = 0;
 
-    $sql = "INSERT INTO leavetable (dateToday, empId, fName, mName, lName, sName, company, manager, dept, position, absenceType, startTime, endTime, pay, leaveType, otherLeaveType, startDate, endDate, reason, dayToHrs) 
-            VALUES ('$dateToday','$empId','$fName', '$mName','$lName','$sName','$company','$manager','$dept','$position','$absenceType','$startTime','$endTime','$pay','$leaveType','$otherLeaveType','$startDateFormatted','$endDateFormatted','$reason','$dayToHrs')";
+$currentDate = clone $startDateTime;
+while ($currentDate <= $endDateTime) {
+    if ($currentDate->format('N') != 7) { // Exclude Sundays
+        $startTime = '00:00:00';
+        $endTime = '23:59:59';
+
+        if ($currentDate->format('Y-m-d') === $startDateTime->format('Y-m-d')) {
+            // First day
+            $startTime = $startDateTime->format('H:i:s');
+        } elseif ($currentDate->format('Y-m-d') === $endDateTime->format('Y-m-d')) {
+            // Last day
+            $endTime = $endDateTime->format('H:i:s');
+        }
+
+        $startOfDay = new DateTime($currentDate->format('Y-m-d') . ' ' . $startTime);
+        $endOfDay = new DateTime($currentDate->format('Y-m-d') . ' ' . $endTime);
+
+        // Calculate hours for the current day
+        $hoursToAdd = min(8, $endOfDay->diff($startOfDay)->h + 1);
+        $totalHours += $hoursToAdd;
+    }
+
+    // Move to the next day
+    $currentDate->modify('+1 day');
+}
+
+    $startDateFormatted = $startDateTime->format('Y-m-d H:i:s');
+    $endDateFormatted = $endDateTime->format('Y-m-d H:i:s');
+
+    $updateStatus = "Pending";   
+    $pay = "Pending";   
+    $remarks = "Pending";    
+
+    $sql = "INSERT INTO jcleavetable (id, dateToday, empId, fName, mName, lName, sName, company, manager, dept, position, absenceType, startTime, endTime, pay, leaveType, otherLeaveType, startDate, endDate, reason, dayToHrs, updateStatus, remarks) 
+            VALUES ('$id', '$dateToday','$empId','$fName', '$mName','$lName','$sName','$company','$manager','$dept','$position','$absenceType','$startDateFormatted','$endDateFormatted','$pay','$leaveType','$otherLeaveType','$startDateFormatted','$endDateFormatted','$reason','$totalHours', '$updateStatus', '$remarks')";
 
     if (mysqli_query($conn, $sql)) {
         echo "Code reaches this point"; // Add this line
-        echo "<script>alert ('Registration Succesfulssssss'); </script>";
+        echo "<script>alert ('Registration Successful!'); </script>";
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
